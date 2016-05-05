78d540a38960a3edcb433bbe01ae21e0ff58dee4
Willymar <willymar.juniarta@gmail.com>
2016-05-05 22:03:28 +0700

    First App

---
+++ app/src/main/java/id/jajaka/pendaftaranmahasiswa/MainActivity.java
@@ -0,0 +1,13 @@
+package id.jajaka.pendaftaranmahasiswa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
