015fb8316a7ad0ad90de242afe456d35fa817e49
Willymar <willymar.juniarta@gmail.com>
2016-05-06 13:52:47 +0700

    add main activity

---
+++ app/src/main/java/id/jajaka/pendaftaranmahasiswa/activity/MainActivity.java
@@ -0,0 +1,25 @@
+package id.jajaka.pendaftaranmahasiswa.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+import id.jajaka.pendaftaranmahasiswa.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Hide titlebar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        // Hide action bar
+        getSupportActionBar().hide();
+
+        setContentView(R.layout.activity_main);
+    }
+}
